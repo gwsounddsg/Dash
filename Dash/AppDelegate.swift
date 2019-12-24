f2b4d514d9b978270598b161d3053d2b2b245f4c
GW <gwsounddsg@gmail.com>
2019-12-24 11:08:22 -0800

    Initial Commit

---
+++ Dash/AppDelegate.swift
@@ -0,0 +1,24 @@
+// ===================================================
+// Created by:  GW Rodriguez
+// Date:        12/24/19
+// Copyright:   Copyright © 2019 GW Rodriguez. All rights reserved.
+// ===================================================
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Dash/ViewController.swift
@@ -0,0 +1,25 @@
+// ===================================================
+// Created by:  GW Rodriguez
+// Date:        12/24/19
+// Copyright:   Copyright © 2019 GW Rodriguez. All rights reserved.
+// ===================================================
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
