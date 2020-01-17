5de8caec4848d8540b754274087c852717743af8
GW Rodriguez <grodriguez@feldinc.com>
2020-01-17 15:43:05 -0500

    removes unneeded prints, fixes server address

--- Dash/Networking/DashOSCServer.swift
+++ Dash/Networking/DashOSCServer.swift
@@ -89,7 +89,6 @@ class DashOSCServer: OSCServerDelegate {
 extension DashOSCServer {
 
     func didReceive(_ message: OSCMessage) {
-        print("OSCServer: \(message)")
         if let msg = getFloatsFrom(message) {
             delegate?.oscDataReceived(msg, type)
         }
