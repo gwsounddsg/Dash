afcc921f6c79718bc010cb97f7614d891b83d3f3
GW Rodriguez <grodriguez@feldinc.com>
2022-10-12 10:11:50 -0400

    add TrackableInfo struct

---
+++ Dash/Model/Trackable.swift
@@ -0,0 +1,16 @@
+//
+// Created by GW Rodriguez on 2/21/20.
+// Copyright (c) 2020 GW Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+
+struct TrackableInfo {
+    let name: String
+    var input: UInt = 0
+    var map: UInt = 1
+}
