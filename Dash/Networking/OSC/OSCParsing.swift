eeabb39811be44a93f8cc9d3c60ed74858ae94e9
GW Rodriguez <gwsounddsg@gmail.com>
2022-11-16 15:58:26 -0500

    add address valid check

--- Dash/Networking/OSC/OSCParsing.swift
+++ Dash/Networking/OSC/OSCParsing.swift
@@ -16,12 +16,37 @@ func OSCParse(_ rawData: Data) throws -> OSCMessage {
 }
 
 
-private func getAddress(_ rawData: Data) -> String {
-    var address = ""
-    var data = rawData
-
+private func getAddress(_ data: Data) -> String {
     let addressEnd = data.firstIndex(of: 0x00)!
-    guard let addressString = data.subdata(in: 0..<addressEnd).toString() else {return address}
+    guard let address = data.subdata(in: 0..<addressEnd).toString() else {return ""}
 
     return address
+}
+
+
+private func isAddressValid(_ address: String) -> Bool {
+    if address == "" {return false}
+    if address.first != "/" {return false}
+    if address.range(of: "/{3,}", options: .regularExpression) != nil {return false} // check for double "//"
+    if address.range(of: "\\s", options: .regularExpression) != nil {return false} // no spaces
+
+    // '[' must be close, no invalid characters inside
+    if address.range(of: "\\[(?![^\\[\\{\\},?\\*/]+\\])", options: .regularExpression) != nil {return false}
+
+    var open = address.components(separatedBy: "[").count
+    var close = address.components(separatedBy: "]").count
+    if open != close {return false}
+
+    //{ must be closed, no invalid characters inside
+    if address.range(of: "\\{(?![^\\{\\[\\]?\\*/]+\\})", options: .regularExpression) != nil {return false}
+
+    open = address.components(separatedBy: "{").count
+    close = address.components(separatedBy: "}").count
+    if open != close {return false}
+
+    //"," only inside {}
+    if address.range(of: ",(?![^\\{\\[\\]?\\*/]+\\})", options: .regularExpression) != nil {return false}
+    if address.range(of: ",(?<!\\{[^\\{\\[\\]?\\*/]+)", options: .regularExpression) != nil {return false}
+
+    return true
 }
