b877b05fd06ebaf4cfed621a0bcf73fd81d3a853
GW Rodriguez <gwsounddsg@gmail.com>
2022-11-18 13:54:21 -0500

    Float extended

---
+++ Dash/Networking/OSC/Extensions/ExtFloat.swift
@@ -0,0 +1,19 @@
+//
+// Created by GW Rodriguez on 11/18/22.
+// Copyright (c) 2022 GW Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+extension Float: OSCType {
+    public var data: Data {
+        get {
+            let bytes: [UInt8] = withUnsafeBytes(of: self.bitPattern.bigEndian, Array.init)
+            return Data(bytes)
+        }
+    }
+
+    init(_ data: Data) {
+        self = Float(bitPattern: UInt32(bigEndian: data.withUnsafeBytes { $0.load(as: UInt32.self) }))
+    }
+}
