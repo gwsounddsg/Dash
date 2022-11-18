871f6e6ad89760cf0a967f49d658ffcb90f5d08f
GW Rodriguez <gwsounddsg@gmail.com>
2022-11-18 13:54:58 -0500

    String extended

---
+++ Dash/Networking/OSC/Extensions/ExtString.swift
@@ -0,0 +1,25 @@
+//
+// Created by GW Rodriguez on 11/18/22.
+// Copyright (c) 2022 GW Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+extension String: OSCType {
+    public var data: Data {
+        get {
+            var data = self.data(using: .utf8)!
+
+            for _ in 1...(4 - data.count%4) {
+                var null = UInt8(0)
+                data.append(&null, count: 1)
+            }
+
+            return data
+        }
+    }
+
+    init(_ data: Data) {
+        self = String(data: data, encoding: .utf8)!
+    }
+}
