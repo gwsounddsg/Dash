ccdc8e79e0f2792bee639aa7a304e6c3d53488c2
GW Rodriguez <gwsounddsg@gmail.com>
2022-11-18 13:53:40 -0500

    Int extended

---
+++ Dash/Networking/OSC/Extensions/ExtInt.swift
@@ -0,0 +1,19 @@
+//
+// Created by GW Rodriguez on 11/18/22.
+// Copyright (c) 2022 GW Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+extension Int: OSCType {
+    public var data: Data {
+        get {
+            let bytes: [UInt8] = withUnsafeBytes(of: Int32(self).bigEndian, Array.init)
+            return Data(bytes)
+        }
+    }
+
+    init (_ data: Data) {
+        self = Int(data.withUnsafeBytes { $0.load(fromByteOffset: 0, as: Int32.self).bigEndian })
+    }
+}
