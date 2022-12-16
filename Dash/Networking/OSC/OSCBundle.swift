f1562f5d8cf0824c66c38ad4f2cede5813161051
GW Rodriguez <gwsounddsg@gmail.com>
2022-12-16 14:52:37 -0500

    OSCBundle members public, add init

--- Dash/Networking/OSC/OSCBundle.swift
+++ Dash/Networking/OSC/OSCBundle.swift
@@ -6,5 +6,27 @@
 import Foundation
 
 public struct OSCBundle {
-    
+    public var timetag: Timetag
+    public var elements: [OSCType] = []
+
+    public var data: Data {
+        get {
+            var data = Data()
+            data.append("#bundle".toBase32())
+            data.append(timetag.data)
+
+            for element in elements {
+                let elementData = element.data
+                data.append(Int32(elementData.count).toData())
+            }
+
+            return data
+        }
+    }
+
+
+    public init(_ elements: OSCType...) {
+        timetag = 1
+        self.elements = elements
+    }
 }
