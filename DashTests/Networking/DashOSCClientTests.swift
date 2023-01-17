6ecffc5673446dc4b5aaf8a5a0ac26339d91c4a3
GW Rodriguez <gwsounddsg@gmail.com>
2023-01-17 15:34:18 -0500

    updated DashOSCClientTests untested

--- DashTests/Networking/DashOSCClientTests.swift
+++ DashTests/Networking/DashOSCClientTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 @testable import Dash
+import Network
 
 
 
@@ -14,18 +15,17 @@ import XCTest
 
 
 class DashOSCClientTests: XCTestCase {
-
-    fileprivate var _client: MockDashOSCClient!
-    let address = "/test/message/"
+    let type: DashNetworkType.Client = .vezer
+    let hostname: String = "127.0.0.1"
     let port: Int = 1234
+    let address = "/test/message/"
+    var client: DashOSCClient!
+    fileprivate var mClient: MockOSCClient!
 
 
     override func setUp() {
-        _client = MockDashOSCClient(.vezer, address, port)
-    }
-
-    override func tearDown() {
-        _client = nil
+        mClient = MockOSCClient()
+        client = DashOSCClient(type, hostname, port, mClient)
     }
 }
 
@@ -36,26 +36,42 @@ class DashOSCClientTests: XCTestCase {
 extension DashOSCClientTests {
 
     func testDashOSCClient() {
-        XCTAssertEqual(_client.type, .vezer)
-        XCTAssertEqual(_client.address, address)
-        XCTAssertEqual(_client.port, port)
-
-        XCTAssertEqual(_client.invokedClientAddressList.count, 0)
-        XCTAssertEqual(_client.invokedClientPortList.count, 0)
+        XCTAssertEqual(client.type, type)
+        XCTAssertEqual(client.address(), hostname)
+        XCTAssertEqual(client.port(), port)
     }
 
 
-    func testDashOSCClient_address() {
-        let newAddy = "/something/different"
-        _client.address = newAddy
-        XCTAssertEqual(_client.invokedClientAddressParameter, newAddy)
+    func testDashOSCClient_clientSend() {
+        let message = OSCMessage("foo", [])
+        client.clientSend(message)
+
+        XCTAssertTrue(mClient.invokedSend)
+        if mClient.invokedSendParameters == nil {return}
+
+        guard let sentMsg = mClient.invokedSendParameters?.element as? OSCMessage else {
+           XCTFail("element is not an OSCMessage")
+        }
+
+        XCTAssertEqual(sentMsg.address, message.address)
     }
 
 
-    func testDashOSCClient_port() {
-        let newPort = 4444
-        _client.port = newPort
-        XCTAssertEqual(_client.invokedClientPortParameter, newPort)
+    func testDashOSCClient_connect_isConnected() {
+        mClient.stubbedIsConnectedResult = true
+
+        client.connect()
+
+        XCTAssertTrue(mClient.invokedDisconnect)
+        XCTAssertTrue(mClient.invokedConnect)
+    }
+
+
+    func testDashOSCClient_connect_isNotConnected() {
+        client.connect()
+
+        XCTAssertFalse(mClient.invokedDisconnect)
+        XCTAssertTrue(mClient.invokedConnect)
     }
 }
 
@@ -63,26 +79,57 @@ extension DashOSCClientTests {
 
 
 
+//MARK: - Getters / Setters
 extension DashOSCClientTests {
 
-    func testDashOSCClient_sendMessage() {
-        let addy = "/send/message/"
+    func testDashOSCClient_port() {
+        let clientPort = client.port()
+        XCTAssertEqual(clientPort, port)
+    }
+
+
+    func testDashOSCClient_setPort() {
+        let newPort = 9876
+        client.setPort(newPort)
+
+        XCTAssertEqual(client.port(), newPort)
+        XCTAssertTrue(mClient.invokedConnect)
+    }
+
+
+    func testDashOSCClient_address() {
+        let clientAddress = client.address()
+        XCTAssertEqual(clientAddress, hostname)
+    }
+
+
+    func testDashOSCClient_setAddress() {
+        let newAddress = "9.9.9.9"
+        client.setAddress(newAddress)
+
+        XCTAssertEqual(client.address(), newAddress)
+        XCTAssertTrue(mClient.invokedConnect)
+    }
+}
+
+
+
+
+
+//MARK: - Sending
+extension DashOSCClientTests {
+
+    func testDashOSCClient_send() {
         let vals: [Float] = [1.0, 2.0]
-        let msg = OSCMessage(addy, vals)
+        let msg = OSCMessage(address, vals)
 
-        _client.send(message: msg)
+        client.send(message: msg)
 
-        if !_client.invokedClientSend {
-            XCTAssertTrue(_client.invokedClientSend)
-            return
+        XCTAssertEqual(mClient.invokedSendCount, 1)
+        XCTAssertNotNil(mClient.invokedSendParameters)
+        if let sentMsg = mClient.invokedSendParameters?.element {
+            XCTAssertEqual(sentMsg.data, msg.data)
         }
-
-        let oscMsg = _client.invokedClientSendParameter as! OSCMessage
-
-        XCTAssertEqual(oscMsg.address, addy)
-        XCTAssertEqual(oscMsg.arguments.count, vals.count)
-        XCTAssertEqual(oscMsg.arguments[0]?.data, vals[0].data)
-        XCTAssertEqual(oscMsg.arguments[1]?.data, vals[1].data)
     }
 
 
@@ -94,12 +141,12 @@ extension DashOSCClientTests {
         let data = DS100(mapping, input: input, x: x, y: y, spread: 0.5)
         let coord: Coordinate = .all
 
-        _client.send(data: [data], coordinate: coord)
+        client.send(data: [data], coordinate: coord)
 
-        XCTAssertTrue(_client.invokedClientSend)
-        if !_client.invokedClientSend {return}
+        XCTAssertTrue(mClient.invokedSend)
+        if !mClient.invokedSend {return}
 
-        let oscBundle = _client.invokedClientSendParameter as! OSCBundle
+        let oscBundle = mClient.invokedSendParameters!.element as! OSCBundle
         XCTAssertEqual(oscBundle.elements.count, 1)
         if oscBundle.elements.count != 1 {return}
 
@@ -123,12 +170,12 @@ extension DashOSCClientTests {
         let data = DS100(mapping, input: input, x: x, y: y, spread: 0.5)
         let coord: Coordinate = .x
         
-        _client.send(data: [data], coordinate: coord)
+        client.send(data: [data], coordinate: coord)
         
-        XCTAssertTrue(_client.invokedClientSend)
-        if !_client.invokedClientSend {return}
+        XCTAssertTrue(mClient.invokedSend)
+        if !mClient.invokedSend {return}
         
-        let oscBundle = _client.invokedClientSendParameter as! OSCBundle
+        let oscBundle = mClient.invokedSendParameters!.element as! OSCBundle
         XCTAssertEqual(oscBundle.elements.count, 1)
         if oscBundle.elements.count != 1 {return}
         
@@ -151,12 +198,12 @@ extension DashOSCClientTests {
         let data = DS100(mapping, input: input, x: x, y: y, spread: 0.5)
         let coord: Coordinate = .y
         
-        _client.send(data: [data], coordinate: coord)
+        client.send(data: [data], coordinate: coord)
         
-        XCTAssertTrue(_client.invokedClientSend)
-        if !_client.invokedClientSend {return}
+        XCTAssertTrue(mClient.invokedSend)
+        if !mClient.invokedSend {return}
         
-        let oscBundle = _client.invokedClientSendParameter as! OSCBundle
+        let oscBundle = mClient.invokedSendParameters!.element as! OSCBundle
         XCTAssertEqual(oscBundle.elements.count, 1)
         if oscBundle.elements.count != 1 {return}
         
@@ -177,23 +224,7 @@ extension DashOSCClientTests {
 
 // MARK: - Mocks
 
-private class MockDashOSCClient: DashOSCClient {
-
-    convenience init() {
-        self.init(<#_#>, "", 0, <#_#>)
-    }
-
-    var invokedClientSend = false
-    var invokedClientSendCount = 0
-    var invokedClientSendParameters: (element: OSCElement, Void)?
-    var invokedClientSendParametersList = [(element: OSCElement, Void)]()
-
-    override func clientSend(_ element: OSCElement) {
-        invokedClientSend = true
-        invokedClientSendCount += 1
-        invokedClientSendParameters = (element, ())
-        invokedClientSendParametersList.append((element, ()))
-    }
+private class MockOSCClient: OSCClient {
 
     var invokedConnect = false
     var invokedConnectCount = 0
@@ -203,34 +234,24 @@ private class MockDashOSCClient: DashOSCClient {
         invokedConnectCount += 1
     }
 
-    var invokedPrintNetwork = false
-    var invokedPrintNetworkCount = 0
+    var invokedConnectTo = false
+    var invokedConnectToCount = 0
+    var invokedConnectToParameters: (address: NWEndpoint.Host, port: NWEndpoint.Port)?
+    var invokedConnectToParametersList = [(address: NWEndpoint.Host, port: NWEndpoint.Port)]()
 
-    override func printNetwork() {
-        invokedPrintNetwork = true
-        invokedPrintNetworkCount += 1
+    override func connect(to address: NWEndpoint.Host, with port: NWEndpoint.Port) {
+        invokedConnectTo = true
+        invokedConnectToCount += 1
+        invokedConnectToParameters = (address, port)
+        invokedConnectToParametersList.append((address, port))
     }
 
-    var invokedPort = false
-    var invokedPortCount = 0
-    var stubbedPortResult: Int! = 0
+    var invokedDisconnect = false
+    var invokedDisconnectCount = 0
 
-    override func port() -> Int {
-        invokedPort = true
-        invokedPortCount += 1
-        return stubbedPortResult
-    }
-
-    var invokedSetPort = false
-    var invokedSetPortCount = 0
-    var invokedSetPortParameters: (newPort: Int, Void)?
-    var invokedSetPortParametersList = [(newPort: Int, Void)]()
-
-    override func setPort(_ newPort: Int) {
-        invokedSetPort = true
-        invokedSetPortCount += 1
-        invokedSetPortParameters = (newPort, ())
-        invokedSetPortParametersList.append((newPort, ()))
+    override func disconnect() {
+        invokedDisconnect = true
+        invokedDisconnectCount += 1
     }
 
     var invokedAddress = false
@@ -243,27 +264,71 @@ private class MockDashOSCClient: DashOSCClient {
         return stubbedAddressResult
     }
 
-    var invokedSetAddress = false
-    var invokedSetAddressCount = 0
-    var invokedSetAddressParameters: (newAddress: String, Void)?
-    var invokedSetAddressParametersList = [(newAddress: String, Void)]()
+    var invokedPort = false
+    var invokedPortCount = 0
+    var stubbedPortResult: Int! = 0
 
-    override func setAddress(_ newAddress: String) {
-        invokedSetAddress = true
-        invokedSetAddressCount += 1
-        invokedSetAddressParameters = (newAddress, ())
-        invokedSetAddressParametersList.append((newAddress, ()))
+    override func port() -> Int {
+        invokedPort = true
+        invokedPortCount += 1
+        return stubbedPortResult
     }
 
-    var invokedChangeEndpoints = false
-    var invokedChangeEndpointsCount = 0
-    var invokedChangeEndpointsParameters: (address: String, port: Int)?
-    var invokedChangeEndpointsParametersList = [(address: String, port: Int)]()
+    var invokedSetEndpoints = false
+    var invokedSetEndpointsCount = 0
+    var invokedSetEndpointsParameters: (address: String, port: Int)?
+    var invokedSetEndpointsParametersList = [(address: String, port: Int)]()
 
-    override func changeEndpoints(address: String, port: Int) {
-        invokedChangeEndpoints = true
-        invokedChangeEndpointsCount += 1
-        invokedChangeEndpointsParameters = (address, port)
-        invokedChangeEndpointsParametersList.append((address, port))
+    override func setEndpoints(address: String, port: Int) {
+        invokedSetEndpoints = true
+        invokedSetEndpointsCount += 1
+        invokedSetEndpointsParameters = (address, port)
+        invokedSetEndpointsParametersList.append((address, port))
+    }
+
+    var invokedUpdateAddress = false
+    var invokedUpdateAddressCount = 0
+    var invokedUpdateAddressParameters: (newAddress: NWEndpoint.Host, Void)?
+    var invokedUpdateAddressParametersList = [(newAddress: NWEndpoint.Host, Void)]()
+
+    override func updateAddress(_ newAddress: NWEndpoint.Host) {
+        invokedUpdateAddress = true
+        invokedUpdateAddressCount += 1
+        invokedUpdateAddressParameters = (newAddress, ())
+        invokedUpdateAddressParametersList.append((newAddress, ()))
+    }
+
+    var invokedUpdatePort = false
+    var invokedUpdatePortCount = 0
+    var invokedUpdatePortParameters: (newPort: NWEndpoint.Port, Void)?
+    var invokedUpdatePortParametersList = [(newPort: NWEndpoint.Port, Void)]()
+
+    override func updatePort(_ newPort: NWEndpoint.Port) {
+        invokedUpdatePort = true
+        invokedUpdatePortCount += 1
+        invokedUpdatePortParameters = (newPort, ())
+        invokedUpdatePortParametersList.append((newPort, ()))
+    }
+
+    var invokedSend = false
+    var invokedSendCount = 0
+    var invokedSendParameters: (element: OSCElement, Void)?
+    var invokedSendParametersList = [(element: OSCElement, Void)]()
+
+    override func send(_ element: OSCElement) {
+        invokedSend = true
+        invokedSendCount += 1
+        invokedSendParameters = (element, ())
+        invokedSendParametersList.append((element, ()))
+    }
+
+    var invokedIsConnected = false
+    var invokedIsConnectedCount = 0
+    var stubbedIsConnectedResult: Bool! = false
+
+    override func isConnected() -> Bool {
+        invokedIsConnected = true
+        invokedIsConnectedCount += 1
+        return stubbedIsConnectedResult
     }
 }
